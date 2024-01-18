9ed85108de5baa7df5cc50267d5b57055c418d87
CreedTrue <34081464+CreedTrue@users.noreply.github.com>
2024-01-17 18:37:06 -0700

    Split the Classes
    Split the 2 files and classes.

---
+++ DiceSimulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//The DiceSimulator class
+// this class does the actual simulation
+class DiceSimulator
+{
+    private static Random random = new Random();
+
+    public int[] SimulateRolls(int numberOfRolls)
+    {
+        int[] results = new int[13];
+
+        // roll the dice the specified number of times
+        for (int i = 0; i < numberOfRolls; i++)
+        {
+            // roll the dice for a random number between 1 and 6
+            int roll1 = random.Next(1, 7);
+            int roll2 = random.Next(1, 7);
+            // increment the appropriate result
+            results[roll1 + roll2]++;
+        }
+
+        return results;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,30 +4,6 @@
 // Dice Rolling Simulator - IS 413 Mission 02
 // This program simulates the rolling of two dice a user-specified number of times.
 
-
-// this class does the actual simulation
-class DiceSimulator
-{
-    private static Random random = new Random();
-
-    public int[] SimulateRolls(int numberOfRolls)
-    {
-        int[] results = new int[13];
-
-        // roll the dice the specified number of times
-        for (int i = 0; i < numberOfRolls; i++)
-        {
-            // roll the dice for a random number between 1 and 6
-            int roll1 = random.Next(1, 7);
-            int roll2 = random.Next(1, 7);
-            // increment the appropriate result
-            results[roll1 + roll2]++;
-        }
-
-        return results;
-    }
-}
-
 // this class is the main program
 class Program
 {
